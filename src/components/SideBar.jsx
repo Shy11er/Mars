05ba84cc9c9f0e31c1ee59e30ed6559f202c89fd
Russian-Idiot <ranil2086@bk.ru>
2023-02-05 14:55:03 +0500

    sidebar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,20 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { links } from "../assets/constants";
-import {
-  HiOutlineHashtag,
-  HiOutlineHome,
-  HiOutlinePhotograph,
-  HiOutlineUserGroup,
-} from "react-icons/hi";
-import { IconContext } from "react-icons";
+
+const NavLinks = ({ isActive, setIsActive }) => (
+  <div className="h-4/5 w-full flex flex-col">
+    {links.map((obj, index) => (
+      <Link
+        to={obj.to}
+        className="flex flex-row justify-start items-center my-4 mx-4 text-sm font-medium text-gray-400 hover:text-cyan-400 cursor-pointer"
+        onClick={() => {
+          setIsActive(index);
+        }}
+      >
+        <obj.icon
+          className={`w-6 h-6 mr-2 ${isActive === index && "text-cyan-400"}`}
+        />
+        <p className={`${isActive === index && "text-cyan-400"}`}>{obj.name}</p>
+      </Link>
+    ))}
+  </div>
+);
 
 const SideBar = () => {
   const [isActive, setIsActive] = React.useState(0);
 
-  const onCl = (ind) => {
-    setIsActive(ind);
-  };
   return (
     <div className="bg-black w-56 h-full flex flex-col justify-center items-center">
       <h1
@@ -23,41 +32,12 @@ const SideBar = () => {
           fontSize: "30px",
           color: "#fff",
           height: "40px",
+          marginBottom: "40px",
         }}
       >
         MARS
       </h1>
-      <div className="h-4/5 w-full flex flex-col mt-[50px]">
-        {links.map((obj, index) => {
-          return (
-            <div key={index} className="flex flex-row w-full h-[40px] p-5">
-              <Link
-                to={obj.to}
-                className="flex flex-row"
-                onClick={() => {
-                  setIsActive(index);
-                }}
-              >
-                <IconContext.Provider
-                  value={{
-                    color: `${isActive === index ? "rgb(34 211 238)" : "#fff"}`,
-                    size: "30px",
-                  }}
-                >
-                  <obj.icon className="hover:text-cyan-400" />
-                </IconContext.Provider>
-                <h1
-                  className={`text-2xl ml-[10px] cursor-pointer hover:text-cyan-400 ${
-                    isActive === index ? "text-cyan-400" : "text-white"
-                  }`}
-                >
-                  {obj.name}
-                </h1>
-              </Link>
-            </div>
-          );
-        })}
-      </div>
+      <NavLinks isActive={isActive} setIsActive={setIsActive} />
     </div>
   );
 };
