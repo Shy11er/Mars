6567634d157a4e543c03aca37586b827e537e802
Russian-Idiot <ranil2086@bk.ru>
2023-02-06 16:09:15 +0500

    search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,39 @@
 import React from "react";
-import search from "../assets/search.svg";
+import { BiSearch } from "react-icons/bi";
+import { AiOutlineClose } from "react-icons/ai";
 
 const Search = () => {
   const [value, setValue] = React.useState("");
 
+  const onSearch = () => {};
+
   return (
-    <div className="relative w-36 h-8">
-      <img src={search} alt="logo" className="absolute" />
-      <input type="text" />
+    <div className="relative w-64 h-8">
+      <BiSearch
+        size={26}
+        className="absolute top-2 left-2 cursor-pointer"
+        onClick={() => {
+          onSearch(value);
+        }}
+      />
+      <input
+        type="text"
+        placeholder="Search..."
+        value={value}
+        onChange={(ev) => {
+          setValue(ev.target.value);
+        }}
+        className="rounded-2xl outline-none border-none text-white px-10 bg-inherit placeholder:text-white focus:outline-none"
+      />
+      {value && (
+        <AiOutlineClose
+          size={18}
+          className="absolute top-3 right-2 cursor-pointer"
+          onClick={() => {
+            setValue("");
+          }}
+        />
+      )}
     </div>
   );
 };
